b3d44fe83edb7b6352b7dbb5b7b3099de63e3857
TDCIAN <tdcian71@gmail.com>
2022-03-14 22:49:06 +0900

    add price textfield cell viewmodel

---
+++ UploadUsedGoods/Presentation/MainViewController/PriceTextFieldCellViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  PriceTextFieldCellViewModel.swift
+//  UploadUsedGoods
+//
+//  Created by JeongminKim on 2022/03/14.
+//
+
+import RxSwift
+import RxCocoa
+
+struct PriceTextFieldCellViewModel {
+    // ViewModel -> View
+    let showFreeShareButton: Signal<Bool>
+    let resetPrice: Signal<Void>
+    
+    // View -> ViewModel
+    let priceValue = PublishRelay<String?>()
+    let freeShareButtonTapped = PublishRelay<Void>()
+    
+    init() {
+        self.showFreeShareButton = Observable
+            .merge(
+                priceValue.map { $0 ?? "" == "0" },
+                freeShareButtonTapped.map { _ in false }
+            )
+            .asSignal(onErrorJustReturn: false)
+        
+        self.resetPrice = freeShareButtonTapped
+            .asSignal(onErrorSignalWith: .empty())
+    }
+}
